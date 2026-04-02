33d43f89ad0f91b96197257c56cb978967dae97a
Aarush Chaudhary <aarushchaudhary153@gmail.com>
2026-04-02 11:32:30 +0530

    s

--- app/src/main/java/com/bioshield/app/network/RetrofitClient.kt
+++ app/src/main/java/com/bioshield/app/network/RetrofitClient.kt
@@ -5,6 +5,10 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import java.security.cert.X509Certificate
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
@@ -42,6 +46,9 @@ object RetrofitClient {
             httpClientBuilder.addInterceptor(AuthInterceptor(provider))
         }
         
+        // Enable HTTPS with self-signed certificate support (for development)
+        configureTrustAllCertificates(httpClientBuilder)
+        
         val httpClient = httpClientBuilder.build()
         
         // Build Retrofit instance
@@ -63,6 +70,29 @@ object RetrofitClient {
         // Rebuild retrofit to apply the new token provider
         if (currentBaseUrl.isNotEmpty()) {
             updateBaseUrl(currentBaseUrl)
+        }
+    }
+
+    /**
+     * Configure OkHttpClient to trust all certificates (for development/self-signed certs)
+     * WARNING: This should only be used in development. Never use in production!
+     */
+    private fun configureTrustAllCertificates(httpClientBuilder: OkHttpClient.Builder) {
+        try {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            })
+
+            val sslContext = SSLContext.getInstance("TLS").apply {
+                init(null, trustAllCerts, java.security.SecureRandom())
+            }
+
+            httpClientBuilder.sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            httpClientBuilder.hostnameVerifier { _, _ -> true } // Trust any hostname
+        } catch (e: Exception) {
+            throw RuntimeException(e)
         }
     }
 }
